d0d79137a251ee6775ad3671251d5445f47bbe51
Toby Mackenzie <public@tobymackenzie.com>
2026-05-07 16:13:32 -0400

    fix: add noop `getSubscribedEvents()` to plugin so non-listening plugins can be created more easily

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,6 +3,7 @@ namespace TJM\Wiki;
 
 abstract class Plugin implements PluginInterface{
 	protected ?Wiki $wiki = null;
+	static public function getSubscribedEvents(): array{ return []; }
 	public function setWiki(Wiki $wiki){
 		$this->wiki = $wiki;
 	}
